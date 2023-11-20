fbd7b24a094d933ffaffce6dd7e1b5dea62b32b2
marigoldjc <chuamarigold@gmail.com>
2023-11-20 19:02:04 +0800

    Intro to Flask Part 1

--- introtoflask_part1/digitalcafe/app.py
+++ introtoflask_part1/digitalcafe/app.py
@@ -1,29 +1,41 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+import database as db
+
 app = Flask(__name__)
-
-navbar = """
-         <a href='/'>Home</a> | <a href='/products'>Products</a> |
-
-         <a href='/branches'>Branches</a> | <a href='/aboutus'>About Us</a>
-         <p/>
-         """
 
 @app.route('/')
 def index():
-    pagecontent = 'Index Page. Place Home Page contents here.'
-    return navbar+pagecontent
+    return render_template('index.html', page="Index")
 
 @app.route('/products')
 def products():
-    pagecontent = 'Products Page. Place Products Page contents here.'
-    return navbar+pagecontent
+    product_list = db.get_products()
+    return render_template('products.html', page="Products", product_list=product_list)
+
+@app.route('/productdetails')
+def productdetails():
+    code = request.args.get('code', '')
+    product = db.get_product(int(code))
+
+    return render_template('productdetails.html', code=code, product=product)
 
 @app.route('/branches')
 def branches():
-    pagecontent = 'Branches Page. Place Branches Page contents here.'
-    return navbar+pagecontent
+    branch_list = db.get_branches()
+    return render_template('branches.html', page="Branches", branch_list=branch_list)
+
+@app.route('/branchdetails')
+def branchdetails():
+    code = request.args.get('code', '')
+    branch = db.get_branch(int(code))
+
+    return render_template('branchdetails.html', code=code, branch=branch)
 
 @app.route('/aboutus')
 def aboutus():
-        pagecontent = 'About Us Page. Place About Us Page contents here.'
-        return navbar+pagecontent
+    return render_template('aboutus.html', page="About Us")
+
+if __name__ == '__main__':
+    app.run(debug=True)
